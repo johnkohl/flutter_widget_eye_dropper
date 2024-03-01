54f32f978662f059c8287106736b339d5e6ff9b8
johnkohl <12026121+johnkohl@users.noreply.github.com>
2024-03-01 11:02:45 -0500

    color values now printed to terminal.

--- lib/main.dart
+++ lib/main.dart
@@ -34,8 +34,15 @@ class _MyHomePageState extends State<MyHomePage> {
   final PixelColorRef ref = PixelColorRef();
 
   // Function to handle hover event
-  void onHover(int x, int y, Color color) async {
-    debugPrint('Hover x: $x, y: $y, color: $color');
+  void onHover(int x, int y, Color color) {
+    // Extract RGBA values
+    int red = color.red;
+    int green = color.green;
+    int blue = color.blue;
+    double opacity = color.opacity; // Alpha value as a fraction of 1
+
+    // Print color values
+    debugPrint('Hover x: $x, y: $y, Color - R:$red, G:$green, B:$blue, Opacity:$opacity');
   }
 
   // Function to handle tap event
